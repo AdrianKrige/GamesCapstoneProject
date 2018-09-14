e288c9eb8bf26745327dcfab9103d377854abec5
Michael White <mike.james.white@icloud.com>
2018-09-14 14:24:48 +0200

    Added weapon stuff to main project

---
+++ Neo Divitias/Assets/Scripts/Combat/WeaponManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+	public Camera playerCamera;
+	private Weapon equipped;
+	private Weapon unequipped;
+
+	void Start()
+	{
+		playerCamera = GetComponent<Camera>();
+	}
+	
+	void Update () {
+		if (Input.GetButton("Fire1"))
+		{
+			equipped.Shoot(playerCamera);
+		}
+		
+		// input check for weapon switching
+		if (Input.GetButton("SwitchWeapon"))
+		{
+			Weapon tmp = equipped;
+			equipped = unequipped;
+			unequipped = tmp;
+		}
+	}
+}
