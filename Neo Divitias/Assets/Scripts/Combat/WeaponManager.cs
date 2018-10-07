062747a6970bf31b0bbac49c2c5913b1d5cd8f80
Adrian Krige <krigeadrian@gmail.com>
2018-10-07 14:10:28 +0200

    Change ui element position. get controller working in game and set first selected in screens

--- Neo Divitias/Assets/Scripts/Combat/WeaponManager.cs
+++ Neo Divitias/Assets/Scripts/Combat/WeaponManager.cs
@@ -16,6 +16,7 @@ public class WeaponManager : MonoBehaviour
 
     private void Start()
     {
+        // How will we determine which player it is? Put them in different layers?
         string primary = GameState.player_one.primary.Split('_')[1];
         switch (primary)
         {
@@ -68,6 +69,4 @@ public class WeaponManager : MonoBehaviour
             //equipped.gameObject.SetActive(true);
         }
 	}
-
-
 }
